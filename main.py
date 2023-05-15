1b3b4ce93a90f60582a7cae4acbf087c596096a6
Gabriel Logan <gabriellogan.0804@gmail.com>
2023-05-15 02:17:27 -0300

    commit2

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ root.withdraw()
 
 count = 0
 
-msg_box = messagebox.showwarning("MOMO", "VOCÊ FOI HACKEADA!")
+msg_box = messagebox.showwarning("NOME DA PESSOA", "VOCÊ FOI HACKEADO!")
 
 if msg_box == 'ok':
-    msg_box = messagebox.showwarning("HAHA!", "PRA SOLUCIONAR ISSO PRECISO QUE VOCÊ RESPONDA ALGUMAS PERGUNTAS")
+    msg_box = messagebox.showwarning("HAHA!", "PRA SOLUCIONAR ISSO PRECISO QUE VOCÊ RESPONDA UMA PERGUNTA")
     
 if msg_box == 'ok':
-    msg_box = messagebox.askquestion("RESPONDA", "VOCÊ AMA SEU NAMORADO PARA SEMPRE?")
+    msg_box = messagebox.askquestion("RESPONDA", "ESCREVA A PERGUNTA")
 
 while msg_box == 'no':
     count += 1
-    msg_box = msg_box = messagebox.askquestion("RESPONDA", "VOCÊ AMA SEU NAMORADO PARA SEMPRE?")
-    if (count == 5):
-        msg_box = messagebox.showerror("VOU TE PEGAR", "VAI TER QUE DIZER SIM HAHAHA")
+    msg_box = msg_box = messagebox.askquestion("RESPONDA", "ESCREVA A PERGUNTA")
+    if (count == 100):
+        msg_box = messagebox.showerror("DIGA A VERDADE", "ABRA DE NOVO E DIGA A VERDADE")
         break
 
 if msg_box == 'yes':
-    msg_box = messagebox.showinfo("BOA!", "ÓTIMA ESCOLHA")    
+    msg_box = messagebox.showinfo("BOA!", "SUA MENSAGEM")    
